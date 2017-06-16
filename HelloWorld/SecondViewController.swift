6c12cfd5d9c5ed5c1fd7633d6951e87517693600
Clement Chan <glorment@gmail.com>
2017-06-16 17:54:03 +0800

    second View Controller

--- HelloWorld/SecondViewController.swift
+++ HelloWorld/SecondViewController.swift
@@ -1,47 +1,42 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  HelloWorld
 //
-//  Created by Clement Chan on 14/6/2017.
+//  Created by Clement Chan on 16/6/2017.
 //  Copyright © 2017 Clement Chan. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
     
-
-    
-    @IBOutlet weak var textToSendField: UITextField!
-
-    @IBAction func showMe(_ sender: Any) {
-        NSLog("use Wrote %@", textToSendField.text! )
-    }
-
-
+    var messageData: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       // lblOutput.text = "Bonjour!"
+
+        messageLabel.text = messageData
         
         
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let messageController = segue.destination as! SecondViewController;
-        
-        messageController.messageData = textToSendField.text;
-        
-        
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-
-
+    */
 
 }
